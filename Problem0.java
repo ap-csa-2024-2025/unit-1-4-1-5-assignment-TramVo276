198b74be8b3d920598c864629d7ac870c49306b6
TramVo276 <tvo941352@gmai.com>
2024-09-10 09:38:17 -0700

    new

--- Problem0.java
+++ Problem0.java
@@ -2,7 +2,7 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-   double = 6;
-   System.out.print(double);
+   double someValue = 6;
+   System.out.print(someValue);
   }
 }
