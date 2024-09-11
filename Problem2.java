b1d590d70c4101f7bc1633b128bff7812636cbde
TramVo276 <tvo941352@gmail.com>
2024-09-11 10:20:53 -0700

    fhhf

--- Problem2.java
+++ Problem2.java
@@ -5,8 +5,8 @@ public class Problem2
         int Num1 = 978;
         System.out.println("Three digit number: " + Num1);
         System.out.println("Here are the digits " + Num1%10);
-        Num2 = Num1%100;
-        System.out.println(Num2);
+        Num1 = Num1%100;
+        System.out.println(Num1);
         
 
         
