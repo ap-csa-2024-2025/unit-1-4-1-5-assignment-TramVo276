e928a4aca19c62c03e8ff6d20bc72dedecc7b955
TramVo276 <tvo941352@gmail.com>
2024-09-12 09:43:23 -0700

    haha

--- Problem2.java
+++ Problem2.java
@@ -3,10 +3,12 @@ public class Problem2
     public static void main(String[] args)
     {
         int Num1 = 978;
-        System.out.println("Three digit number: " + Num1);
-        System.out.println("Here are the digits " + Num1%10);
-        Num1 = Num1%100;
         System.out.println(Num1);
+        Num1 /= 10
+        System.out.println(Num1);
+        Num1 /= 10
+        System.out.println(Num1);
+        Num1 /= 10
         
 
         
